4a5ee07b2efb7b85ef13490cc4bff144b6fe1b53
Vikas Gupta <vg733834@gmail.com>
2023-01-29 19:54:38 +0530

    Worked on main and blog activity and layout

---
+++ app/src/main/java/com/example/bloggingapp/BlogActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bloggingapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BlogActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.blog)
+    }
+}
--- app/src/main/java/com/example/bloggingapp/MainActivity.kt
+++ app/src/main/java/com/example/bloggingapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.bloggingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn=findViewById<Button>(R.id.blogbtn1)
+        val btn2=findViewById<Button>(R.id.blogbtn2)
+        val btn3=findViewById<Button>(R.id.blogbtn3)
+
+        val i = Intent(applicationContext,BlogActivity::class.java)
+
+        btn.setOnClickListener {
+            startActivity(i);
+        }
+        btn2.setOnClickListener {
+            startActivity(i);
+        }
+        btn3.setOnClickListener {
+            startActivity(i);
+        }
     }
+
 }
